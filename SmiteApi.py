83fde504a0905d827124ebea8d02b510772509ec
Leendert Coenen <CLeendert@gmail.com>
2020-12-31 13:56:24 +0100

    GetRank finished, with enum

--- SmiteApi.py
+++ SmiteApi.py
@@ -1,39 +1,25 @@
-import pyrez
-from pyrez.api import *
-from pyrez.exceptions import (
-    PlayerNotFound,
-    MatchException,
-)
-from pyrez.exceptions.PrivatePlayer import PrivatePlayer
-from pyrez.enumerations import Tier
 from pyrez import SmiteAPI
-import json
-from pyrez.enumerations.QueueSmite import QueueSmite
-
-# from ..utils import get_env
-# from ..utils.num import format_decimal
-# from langs import *
-
+from pyrez.enumerations import Tier
 import requests
 from DiscordBot.env_vars import devId, authKey
 
-# def connecteren:
-smiteAPI = SmiteAPI(devId=devId, authKey=authKey)
-
 
 class LastMatch:
+
     def __init__(self, player_name):
+        self.smiteAPI = SmiteAPI(devId=devId, authKey=authKey)
         self.player_name = player_name
 
-    def NameToId(self):
-        player_id = smiteAPI.getPlayerId(self.player_name, portalId=None)
+    def player_id(self):
+        player_id = self.smiteAPI.getPlayerId(self.player_name, portalId=None)
         return player_id[0].playerId
 
-    def MatchHistory(self):
-        LastGame = smiteAPI.getMatchHistory(playerId=self.player_id)
-        return LastGame
+    def conquest_rank(self):
+        conquestrank = self.smiteAPI.getPlayer(self.player_id(), portalId=None)
+        rank_number = conquestrank.rankedConquest['Tier']
+        rank = Tier(rank_number).name
+        return rank
 
 
 cleendert = LastMatch('Cleendert')
-
-print(cleendert.NameToId())
+print(cleendert.conquest_rank())
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-testdict = {"player_id": 355709,
-  "portal": "Steam",
-  "portal_id": "5",
-  "privacy_flag": "n",
-  "ret_msg": 'null'
-}
+from pyrez import SmiteAPI
+import json
+import requests
+from DiscordBot.env_vars import devId, authKey
 
+# def connecteren:
+smiteAPI = SmiteAPI(devId=devId, authKey=authKey)
 
-print(testdict[0])
+player_id = smiteAPI.getPlayerId('cleendert', portalId=None)
+print(player_id[0].playerId)
