c33a32179804e69251e69d8aee4ad0bd55e4b03b
Leendert Coenen <CLeendert@gmail.com>
2021-01-12 23:12:55 +0100

    Add god names

--- GetRank.py
+++ GetRank.py
@@ -32,6 +32,11 @@ class LastMatch:
         except PyrezException:
             return ('Oei! er is iets misgelopen')
 
+    def god_name(self):
+        matchhistory = self.smiteAPI.getMatchHistory(self.player_id())
+        god_name = matchhistory[0].godName
+        return god_name
+
     # Gets last Match Id's
     def last_match_id(self):
         # conquest = 426
@@ -45,63 +50,71 @@ class LastMatch:
         return names
 
     def last_match_ranks(self):
+        Godnames = []
         names = self.last_match_playerNames()
         ranks = []
-        Pranks = {}
         for player in names:
             ranks.append(LastMatch(player).conquest_rank())
-        # print(ranks)
-        # print(names)
+            Godnames.append(LastMatch(player).god_name())
+        Pranks = {}
+        #TODO: This is the last god played, needs to be connected to the match id
+        GodNamesOfPlayers = {}
+
         for name in names:
             for rank in ranks:
                 Pranks[name] = rank
                 ranks.remove(rank)
+                break
+        for name in names:
+            for godname in Godnames:
+                GodNamesOfPlayers[name] = godname
+                Godnames.remove(godname)
                 break
         # Divide Teams
         T1p = list(Pranks.keys())[:5]
         T1r = list(Pranks.values())[:5]
         T2p = list(Pranks.keys())[5:]
         T2r = list(Pranks.values())[5:]
+        Team1GodName = list(GodNamesOfPlayers.values())[:5]
+        Team2GodName = list(GodNamesOfPlayers.values())[5:]
+        # TODO Add godnames
         Teams = f"""
-         #TODO Add godnames
-    Enemy Team:
-        {T1p[0]}   is  {T1r[0]}
-        {T1p[1]}   is  {T1r[1]}
-        {T1p[2]}   is  {T1r[2]}
-        {T1p[3]}   is  {T1r[3]}
-        {T1p[4]}   is  {T1r[4]}
+    Blue Team:
+        {T1p[0]}   is  {T1r[0]} as {Team1GodName[0]}
+        {T1p[1]}   is  {T1r[1]} as {Team1GodName[1]}
+        {T1p[2]}   is  {T1r[2]} as {Team1GodName[2]}
+        {T1p[3]}   is  {T1r[3]} as {Team1GodName[3]}
+        {T1p[4]}   is  {T1r[4]} as {Team1GodName[4]}
         
-    Your Team:  
-        {T2p[0]}   is  {T2r[0]}
-        {T2p[1]}   is  {T2r[1]}
-        {T2p[2]}   is  {T2r[2]}
-        {T2p[3]}   is  {T2r[3]}
-        {T2p[4]}   is  {T2r[4]}
+    Red Team:  
+        {T2p[0]}   is  {T2r[0]} as {Team2GodName[0]}
+        {T2p[1]}   is  {T2r[1]} as {Team2GodName[1]}
+        {T2p[2]}   is  {T2r[2]} as {Team2GodName[2]}
+        {T2p[3]}   is  {T2r[3]} as {Team2GodName[3]}
+        {T2p[4]}   is  {T2r[4]} as {Team2GodName[4]}
         
         """
 
         return (Teams)
 
 
-class TopItems:
-    def __init__(self, god_name):
-        self.god_name = god_name
-        
-    def most_build(self):
+# class TopItems:
+#     def __init__(self, god_name):
+#         self.god_name = god_name
+#
+#     def most_build(self):
 
-     URL = f" https://smite.guru/builds/{self.god_name}"
-     page = requests.get(URL)
-     soup = BeautifulSoup(page.content,('html.parser'))
-     build = soup.find(class_='item-img')
-     print(build)
-     return build
-  #when returns none, it's not a known god, at error handling
+# URL = f" https://smite.guru/builds/{self.god_name}"
+# page = requests.get(URL)
+# soup = BeautifulSoup(page.content,('html.parser'))
+# build = soup.find(class_='item-img')
+# print(build)
+# return build
+# when returns none, it's not a known god, at error handling
 
-    # def test(self):
-    #     matchhistory = self.smiteAPI.getMatchHistory(self.player_id())
-    #     print(matchhistory)
+
 # cleendert = LastMatch('Cleendert')
-# print(cleendert.test())
+# print(cleendert.last_match_ranks())
 
-cleendert = TopItems('anubis')
-cleendert.most_build()
+# cleendert = TopItems('anubis')
+# cleendert.most_build()
