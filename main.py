7e1e29fccb33509bf4535612b5f67b0056d1df00
Leendert Coenen <CLeendert@gmail.com>
2021-01-17 23:40:54 +0100

    model in pickle

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import tflearn
 import tensorflow as tf
 import random
 import json
+import pickle
 
 
 nltk.download('punkt')
@@ -18,49 +19,57 @@ SENT_DETECTOR = nltk.data.load('tokenizers/punkt/english.pickle')
 # Deep learning
 with open("intents.json") as file:
     data = json.load(file)
-words = []
-labels = []
-docs_x = []
-docs_y = []
+#Put an X in try when intent data has changed, or delete old model.
+try:
+    with open("data.pickle", "rb") as f:
+        words,labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(["tag"])
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
 
-    if intent["tag"] not in labels:
-        labels.append(intent["tag"])
-words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
-words = sorted(list(set(words)))
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+    words = sorted(list(set(words)))
 
-labels = sorted(labels)
+    labels = sorted(labels)
 
-training = []
-output = []
+    training = []
+    output = []
 
-out_empty = [0 for _ in range(len(labels))]
+    out_empty = [0 for _ in range(len(labels))]
 
-for x, doc in enumerate(docs_x):
-    bag = []
+    for x, doc in enumerate(docs_x):
+        bag = []
 
-    wrds = [stemmer.stem(w) for w in doc]
+        wrds = [stemmer.stem(w) for w in doc]
 
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
 
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
-    training.append(bag)
-    output.append(output_row)
-# Take lists and turn them into arrays
-training = numpy.array(training)
-output = numpy.array(output)
+        training.append(bag)
+        output.append(output_row)
+    # Take lists and turn them into arrays
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
 
 # tf.reset_default_graph()
 
@@ -74,12 +83,15 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
+try:
+    model.load("model.tflearn")
 
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
 
-
-
+def bag_of_words(s, words):
+    
 
 
 # SmiteAPI + Discord Bot
